5aeb97d5d015eed8df9cecedeace19acb1027d25
靳荣达 <2368741842@qq.com>
2022-08-22 17:17:21 +0800

    提取轮播图为全局组件

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import router from '@/router'
 import axios from 'axios';
 
 import TypeNav from '@/components/TypeNav';
-
+import Carousel from '@/components/Carousel';
 import store from '@/store';
 //执行mock文件，创建虚拟接口
 import "@/mock/mockServe.js";
 //引入轮播图样式
 import "swiper/css/swiper.css"
 Vue.component(TypeNav.name,TypeNav);
+Vue.component(Carousel.name,Carousel);
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false
 // //测试请求是否发送成功
--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -4,23 +4,7 @@
     <div class="sortList clearfix">
       <div class="center">
         <!--banner轮播-->
-        <div class="swiper-container" ref="mySwiper">
-          <div class="swiper-wrapper">
-            <div
-              class="swiper-slide"
-              v-for="carousel in bannerList"
-              :key="carousel.id"
-            >
-              <img :src="carousel.imgUrl" />
-            </div>
-          </div>
-          <!-- 如果需要分页器 -->
-          <div class="swiper-pagination"></div>
-
-          <!-- 如果需要导航按钮 -->
-          <div class="swiper-button-prev"></div>
-          <div class="swiper-button-next"></div>
-        </div>
+        <Carousel :list="bannerList" />
       </div>
       <div class="right">
         <div class="news">
@@ -97,7 +81,6 @@
 
 <script>
 import { mapState } from "vuex";
-import Swiper from "swiper";
 export default {
   name: "ListContainer",
   mounted() {
@@ -107,32 +90,6 @@ export default {
     ...mapState({
       bannerList: (state) => state.home.bannerList,
     }),
-  },
-  watch: {
-    bannerList() {
-      this.$nextTick(() => {
-        var mySwiper = new Swiper(this.$refs.mySwiper, {
-          direction: "horizontal", // 垂直切换选项
-          loop: true, // 循环模式选项
-
-          // 如果需要分页器
-          pagination: {
-            el: ".swiper-pagination",
-          },
-
-          // 如果需要前进后退按钮
-          navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          },
-
-          // 如果需要滚动条
-          scrollbar: {
-            el: ".swiper-scrollbar",
-          },
-        });
-      });
-    },
   },
 };
 </script>
