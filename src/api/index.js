77125796a32d76a48f003f4a454a02c74b7e0446
靳荣达 <2368741842@qq.com>
2022-08-25 09:40:09 +0800

    获取search组件的数据（需调用vuex的getSearchList）

--- src/api/index.js
+++ src/api/index.js
@@ -15,3 +15,11 @@ export const reqGetBannerList = () => mockRequest.get('/banner');
 
 //请求Floor组件数据的函数
 export const reqGetFloorList = () => mockRequest.get('/floor');
+
+//请求Search组件数据:调用axios方法发送请求(需要一个配置对象,去官网查看配置对象的属性)
+//axios方法的配置对象的data项必须有，起码是个空对象，所以调用reqGetSearchInfo函数时，至少传入一个空对象
+export const reqGetSearchInfo = (params) => requests({
+    url:"/list",
+    method:"post",
+    data:params,
+})
