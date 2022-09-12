feadb502012b10c5f846c80d50d6b309a7e4ba53
靳荣达 <2368741842@qq.com>
2022-09-12 23:57:28 +0800

    Detail路由组件商品售卖属性动态展示

--- src/store/detail/index.js
+++ src/store/detail/index.js
@@ -17,12 +17,18 @@ const actions = {
     }
 };
 const getters = {
+    //Detail左上角路径导航信息的简化
     categoryView(state) {
         //goodInfo中并不一定存在（请求失败时）categoryView，所以或一个空对象
         return state.goodInfo.categoryView||{};
     },
+    //产品信息的简化
     skuInfo(state) {
         return state.goodInfo.skuInfo||{};
+    },
+    //产品售卖属性的简化
+    spuSaleAttrList(state) {
+        return state.goodInfo.spuSaleAttrList||[];
     }
 };
 export default {
