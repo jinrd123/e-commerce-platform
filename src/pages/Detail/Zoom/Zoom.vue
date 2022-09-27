d6fa417e1ebdf0b66a93478f52e439a66bdee905
靳荣达 <2368741842@qq.com>
2022-09-27 09:16:40 +0800

    放大区域图片与轮播图展示一致、放大镜绿色遮罩层与放大区域的实时移动

--- src/pages/Detail/Zoom/Zoom.vue
+++ src/pages/Detail/Zoom/Zoom.vue
@@ -2,12 +2,15 @@
   <div class="spec-preview">
     <!-- 左上大图 -->
     <img :src="imgObj.imgUrl" />
-    <div class="event"></div>
+    <!-- 绑定事件的元素 -->
+    <div class="event" @mousemove="handler"></div>
+    <!-- 右侧放大图 -->
     <div class="big">
       <!-- 放大镜里的图片，和左上大图一样 -->
-      <img :src="imgObj.imgUrl" />
+      <img :src="imgObj.imgUrl" ref="big"/>
     </div>
-    <div class="mask"></div>
+    <!-- 绿色放大镜遮罩层 -->
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
@@ -15,9 +18,50 @@
   export default {
     name: "Zoom",
     props:['skuImageList'],
+    data() {
+      return {
+        currentIndex: 0,
+      }
+    },
     computed:{
       imgObj() {
-        return this.skuImageList[0]||{};
+        return this.skuImageList[this.currentIndex]||{};
+      }
+    },
+    mounted() {
+      //全局事件总线，获取兄弟组件(下面的轮播图列表)传递过来的索引值
+      this.$bus.$on("getIndex", (index) => {
+        this.currentIndex = index;
+      })
+    },
+    methods: {
+      handler(event) {
+        let mask = this.$refs.mask;
+        let big = this.$refs.big;
+        let left = event.offsetX - mask.offsetWidth/2;
+        let top = event.offsetY - mask.offsetHeight/2;
+        //限制遮罩层不出界
+        //左界
+        if(left < 0) {
+          left = 0;
+        }
+        //右界
+        if(left + mask.offsetWidth >= event.target.offsetWidth) {
+          left = event.target.offsetWidth - mask.offsetWidth;
+        }
+        //上界
+        if(top < 0) {
+          top = 0;
+        }
+        //下界
+        if(top + mask.offsetHeight > event.target.offsetHeight) {
+          top = event.target.offsetHeight - mask.offsetHeight;
+        }
+        //修改遮罩层的left|top属性值;同时修改放大区域的left|top属性值(放大镜相关逻辑并不明白，只知道修改相关定位属性即可)
+        mask.style.left = left+"px";
+        mask.style.top = top+"px";
+        big.style.top = -2*top+"px";
+        big.style.left = -2*left+"px";
       }
     }
   }
