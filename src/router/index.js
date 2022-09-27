225cce53d7b9ddbda4804d588dc919316eb06ad8
靳荣达 <2368741842@qq.com>
2022-09-27 22:48:23 +0800

    添加购物车静态组件

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Detail from '@/pages/Detail';
 import AddCartSuccess from '@/pages/AddCartSuccess';
+import ShopCart from '@/pages/ShopCart';
 
 //先把VueRouter原型对象的push方法的函数体保存一份
 let originPush = VueRouter.prototype.push;
@@ -36,6 +37,13 @@ VueRouter.prototype.replace = function (location, resolve, reject) {
 //配置路由
 export default new VueRouter({
     routes: [
+        {
+            path: "/shopcart",
+            component: ShopCart,
+            meta: {
+                show: true,
+            },
+        },
         {
             path: "/addcartsuccess",
             name: 'addcartsuccess',
