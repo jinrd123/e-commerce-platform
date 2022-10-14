f027b411b9733d0f3efc2ecabb2ceaf75cdc1a05
靳荣达 <2368741842@qq.com>
2022-10-15 00:06:34 +0800

    trade结算组件展示完成

--- src/pages/Trade/index.vue
+++ src/pages/Trade/index.vue
@@ -29,40 +29,24 @@
       </div>
       <div class="detail">
         <h5>商品清单</h5>
-        <ul class="list clearFix">
+        <ul class="list clearFix" v-for="order in orderInfo.detailArrayList" :key="order.skuId">
           <li>
-            <img src="./images/goods.png" alt="">
+            <img :src="order.imgUrl" style="width:100px;height:100px">
           </li>
           <li>
-            <p>
-              Apple iPhone 6s (A1700) 64G 玫瑰金色 移动联通电信4G手机硅胶透明防摔软壳 本色系列</p>
+            <p>{{order.skuName}}</p>
             <h4>7天无理由退货</h4>
           </li>
           <li>
-            <h3>￥5399.00</h3>
+            <h3>￥{{order.orderPrice}}.00</h3>
           </li>
-          <li>X1</li>
-          <li>有货</li>
-        </ul>
-        <ul class="list clearFix">
-          <li>
-            <img src="./images/goods.png" alt="">
-          </li>
-          <li>
-            <p>
-              Apple iPhone 6s (A1700) 64G 玫瑰金色 移动联通电信4G手机硅胶透明防摔软壳 本色系列</p>
-            <h4>7天无理由退货</h4>
-          </li>
-          <li>
-            <h3>￥5399.00</h3>
-          </li>
-          <li>X1</li>
+          <li>X{{order.skuNum}}</li>
           <li>有货</li>
         </ul>
       </div>
       <div class="bbs">
         <h5>买家留言：</h5>
-        <textarea placeholder="建议留言前先与商家沟通确认" class="remarks-cont"></textarea>
+        <textarea placeholder="建议留言前先与商家沟通确认" class="remarks-cont" v-model="msg"></textarea>
 
       </div>
       <div class="line"></div>
@@ -76,7 +60,7 @@
       <ul>
         <li>
           <b><i>1</i>件商品，总商品金额</b>
-          <span>¥5399.00</span>
+          <span>¥{{orderInfo.totalAmount}}.00</span>
         </li>
         <li>
           <b>返现：</b>
@@ -89,7 +73,7 @@
       </ul>
     </div>
     <div class="trade">
-      <div class="price">应付金额:　<span>¥5399.00</span></div>
+      <div class="price">应付金额:　<span>¥{{orderInfo.totalAmount}}.00</span></div>
       <div class="receiveInfo">
         寄送至:
         <span>{{userDefaultAddress.fullAddress}}</span>
@@ -113,10 +97,11 @@ import {mapState} from 'vuex';
     },
     computed: {
       ...mapState({
-        addressInfo:state=>state.trade.address
+        addressInfo:state=>state.trade.address,
+        orderInfo:state=>state.trade.orderInfo
       }),
       userDefaultAddress() {
-        return this.addressInfo.find((item)=>item.isDefault == 1);
+        return this.addressInfo.find((item)=>item.isDefault == 1)||{};
       }
     },
     methods: {
@@ -125,6 +110,11 @@ import {mapState} from 'vuex';
         //排他操作
         addressInfo.forEach(item=>item.isDefault=0);
         address.isDefault = 1;
+      }
+    },
+    data() {
+      return {
+        msg: '',
       }
     }
   }
